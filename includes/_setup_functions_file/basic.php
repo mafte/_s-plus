023282d5ca7b532f2ccb82bb8472b14c3881875b
mafte <maca19952002@gmail.com>
2021-11-26 16:34:26 -0600

    Add automatic version in queued files

--- includes/_setup_functions_file/basic.php
+++ includes/_setup_functions_file/basic.php
@@ -4,6 +4,33 @@ if (!defined('_S_VERSION')) {
     define('_S_VERSION', '1.0.0');
 }
 
+if (!defined('_SP_THEME_D')) {
+    define('_SP_THEME_D', get_template_directory());
+}
+if (!defined('_SP_THEME_U')) {
+    define('_SP_THEME_U', get_template_directory_uri());
+}
+if (!defined('_SP_STYLE_D')) {
+    define('_SP_STYLE_D', get_template_directory() . '/assets/dist/css/');
+}
+if (!defined('_SP_STYLE_U')) {
+    define('_SP_STYLE_U', get_template_directory_uri() . '/assets/dist/css/');
+}
+if (!defined('_SP_SCRIPTS_D')) {
+    define('_SP_SCRIPTS_D', get_template_directory() . '/assets/dist/js/');
+}
+if (!defined('_SP_SCRIPTS_U')) {
+    define('_SP_SCRIPTS_U', get_template_directory_uri() . '/assets/dist/js/');
+}
+if (!defined('_SP_IMG_D')) {
+    define('_SP_IMG_D', get_template_directory() . '/assets/dist/img/');
+}
+if (!defined('_SP_IMG_U')) {
+    define('_SP_IMG_U', get_template_directory_uri() . '/assets/dist/img/');
+}
+
+
+
 if (!function_exists('s_plus_setup')) :
     /**
      * Sets up theme defaults and registers support for various WordPress features.
--- includes/_setup_functions_file/scripts_and_styles.php
+++ includes/_setup_functions_file/scripts_and_styles.php
@@ -1,9 +1,14 @@
 <?php
 function s_plus_scripts() {
-    wp_enqueue_style('s-plus-style', get_stylesheet_uri(), array(), _S_VERSION);
+
+    /*	|> STYLES
+    \*------------------------------------------------------*/
+    wp_enqueue_style('s-plus-style', get_stylesheet_uri(), array(), '1.' . filemtime(_SP_THEME_D . '/style.css'));
     wp_style_add_data('s-plus-style', 'rtl', 'replace');
 
-    wp_enqueue_script('s-plus-navigation', get_template_directory_uri() . '/assets/source/js/navigation.js', array(), _S_VERSION, true);
+    /*	|> SCRIPTS
+    \*------------------------------------------------------*/
+    wp_enqueue_script('s-plus-navigation', _SP_THEME_U . '/assets/source/js/navigation.js', array(), '1.' . filemtime(_SP_THEME_D . '/assets/source/js/navigation.js'), true);
 
     if (is_singular() && comments_open() && get_option('thread_comments')) {
         wp_enqueue_script('comment-reply');
