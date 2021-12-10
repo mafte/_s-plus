868a26c70971320b584c6df2bc09d13159626087
mafte <maca19952002@gmail.com>
2021-12-09 19:28:56 -0600

    Fix function name

--- ACF/flexible-content/cp-image.php
+++ ACF/flexible-content/cp-image.php
@@ -8,7 +8,7 @@
 <div class="cp-image <?php echo $cp->class_css; ?>">
     <?php $imageID =  $cp->image ?>
 
-    <img class="cp-image__item img-fluid-fix" src="<?php echo sp_get_img__url('large', $imageID); ?>" alt="<?php echo getImg__alt($imageID); ?>">
+    <img class="cp-image__item img-fluid-fix" src="<?php echo sp_get_img__url('large', $imageID); ?>" alt="<?php echo sp_get_img__alt($imageID); ?>">
 </div>
 
 <!-- CP-IMAGE ************************** -->
