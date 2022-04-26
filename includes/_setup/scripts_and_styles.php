303c1039791b4291dabfcf761a879753a316715e
mafte <maca19952002@gmail.com>
2022-04-26 09:20:25 -0600

    Update scripts enqueue

--- includes/_setup/scripts_and_styles.php
+++ includes/_setup/scripts_and_styles.php
@@ -12,15 +12,15 @@ function _s_plus_scripts() {
     // wp_enqueue_style('fluid-spacing', $url_styles . 'spacing-fluid-min.css', array(), '1.' . filemtime($dir_styles . 'spacing-fluid-min.css'));
     // wp_enqueue_style('tiny-slider-styles', $url_styles . 'tiny-slider.css', array(), '1');
     wp_enqueue_style('_s_plus-style', get_stylesheet_uri(), array(), '1.' . filemtime(get_template_directory() . '/style.css'));
-    wp_style_add_data('_s_plus-style', 'rtl', 'replace');
+    // wp_style_add_data('_s_plus-style', 'rtl', 'replace');
 
     /*	|> SCRIPTS
     \*------------------------------------------------------*/
     wp_enqueue_script('lazyload', $url_scripts . 'lazyload.min.js', array(), '1', true);
     wp_enqueue_script('_s_plus-main', get_template_directory_uri() . '/assets/dist/js/production.min.js', array(), '1.' . filemtime(get_template_directory() . '/assets/dist/js/production.min.js'), true);
 
-    if (is_singular() && comments_open() && get_option('thread_comments')) {
-        wp_enqueue_script('comment-reply');
-    }
+    // if (is_singular() && comments_open() && get_option('thread_comments')) {
+    //     wp_enqueue_script('comment-reply');
+    // }
 }
 add_action('wp_enqueue_scripts', '_s_plus_scripts');
