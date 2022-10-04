b96a84b417bea33e5481db05f6587cba91fa5b25
mafte <maca19952002@gmail.com>
2022-10-04 17:43:37 -0600

    Update files

--- includes/_setup/scripts_and_styles.php
+++ includes/_setup/scripts_and_styles.php
@@ -10,14 +10,12 @@ function _s_plus_scripts() {
     /*  STYLES
     ——————————————————————————————————————————————————————*/
 
-    // wp_enqueue_style('fluid-spacing', $url_styles . 'spacing-fluid-min.css', array(), '1.' . filemtime($dir_styles . 'spacing-fluid-min.css'));
-    // wp_enqueue_style('tiny-slider-styles', $url_styles . 'tiny-slider.css', array(), '1');
     wp_enqueue_style('_s_plus-style', get_stylesheet_uri(), array(), '1.' . filemtime(get_template_directory() . '/style.css'));
     // wp_style_add_data('_s_plus-style', 'rtl', 'replace');
 
     /*  SCRIPTS
     ——————————————————————————————————————————————————————*/
-    wp_enqueue_script('lazyload', $url_scripts . 'lazyload.min.js', array(), '1', true);
+    wp_enqueue_script('lazyload', $url_scripts . 'lazyload.min.js', array(), '17.8.3', true);
     wp_enqueue_script('_s_plus-main', get_template_directory_uri() . '/assets/dist/js/production.min.js', array(), '1.' . filemtime(get_template_directory() . '/assets/dist/js/production.min.js'), true);
 
     // if (is_singular() && comments_open() && get_option('thread_comments')) {
