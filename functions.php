6e7ee39afe40b1222923561fa19d70d82cf26b09
mafte <maca19952002@gmail.com>
2024-05-13 20:32:34 -0600

    Add rest_authentication function

--- functions.php
+++ functions.php
@@ -48,3 +48,31 @@ require_once 'includes/features/acf.php';
 \*————————————————————————————————————————————————————*/
 
 require_once 'includes/features/improve-wp-nav.php';
+
+
+/*  |> REST API disabled for unauthenticated users
+——————————————————————————————————————————————————————*/
+add_filter(
+	'rest_authentication_errors',
+	function ( $result ) {
+		// If a previous authentication check was applied,
+		// pass that result along without modification.
+		if ( true === $result || is_wp_error( $result ) ) {
+			return $result;
+		}
+
+		// No authentication has been performed yet.
+		// Return an error if user is not logged in.
+		if ( ! is_user_logged_in() ) {
+			return new WP_Error(
+				'rest_not_logged_in',
+				__( 'You are not currently logged in.' ),
+				array( 'status' => 401 )
+			);
+		}
+
+		// Our custom authentication check should have no effect
+		// on logged-in requests
+		return $result;
+	}
+);
